5a640994245383e535d6b0a8714685df6a7d1768
Terje Topland <terjetop@uia.no>
2023-02-06 11:35:21 +0100

    working on new tutorial react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styled from './testOne.css';
+
+const firstElement = <h1 className="firstElementStyle">Hello again!</h1>
+
+class Room {
+    constructor(name) {
+        this.roomName = name;
+    }
+    nameOfRoom() {
+        return 'You are now viewing room ' + this.roomName;
+    }
+}
+class Wall extends Room{
+    constructor(name, thickness) {
+        // super() refers to parent class
+        super(name);
+        this.wall = thickness;
+    }
+    viewWallThickness(){
+        return 'Size of wall is ' + this.wall;
+    }
+
+    viewAllInfoOfWall() {
+        return this.nameOfRoom() + ' ' + this.viewWallThickness();
+    }
+}
+
+
+
+const wallOne = new Wall('Sleepingroom', 100);
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(wallOne.viewAllInfoOfWall());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
